e9b42eda03157900f0cb6eb0deaf5b917ac41d81
tatanka22 <harald.grosvik@fritun.no>
2023-12-10 12:06:27 +0100

    new gui

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from requests import get
 import sys
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5.QtCore import QTimer, QTime, QDate
+from PyQt5.QtCore import QTimer, QTime, QDate, QRegExp
+from PyQt5.QtGui import QRegExpValidator
 from MyGui import Ui_MainWindow
 
 class MyWindow(QMainWindow, Ui_MainWindow):
@@ -10,33 +11,36 @@ class MyWindow(QMainWindow, Ui_MainWindow):
         QtWidgets.QMainWindow.__init__(self)
         Ui_MainWindow.__init__(self)
         self.setupUi(self)
-        timer1 = QTimer(self)  # Timer for å vise tiden. millisec.
+        # Timer for å oppatere vinduet og vise tiden. millisec.
+        timer1 = QTimer(self)  
         timer1.timeout.connect(self.visTid)
         timer1.start(200)
-        self.SetBtn.clicked.connect(self.set_button_clicked)
-        self.iptid = 120
+        # Knapp for å sette tiden i sekunder mellom hver ip-sjekk
+        self.btn_set_interval.clicked.connect(self.set_button_clicked)
+        self.ip_tid = 120
         self.lineEdit.setText("120")
+        # self.lineEdit.textChanged.connect(self.test_change)
+        # Validator
+        validator = QRegExpValidator(QRegExp(r'[0-9]+'))
+        self.lineEdit.setValidator(validator)
+        # Diverse
         current_time = QTime.currentTime()
         current_date = QDate.currentDate()
-        self.lastTime = current_time
-        self.nextTime = current_time
-        self.lastIpDate = "12.12.12"
-        self.lastIpTime = "00:00:00"
-        self.lastIp = "192.192.192.192"
-        self.label_upsince_val.setText(current_time.toString())
+        self.last_time = current_time
+        self.next_time = current_time
+        self.lbl_upsince_time_val.setText(current_time.toString('hh:mm:ss'))
         self.lbl_upsince_date_val.setText(current_date.toString('dd.MM.yyyy'))
-        #test
-
-
-    #ny funk
+        self.last_ip_date = "12.12.12"
+        self.last_ip_time = "00:00:00"
+        self.last_ip = "192.192.192.192"
     
     def set_button_clicked(self):
-        self.iptid = int(self.lineEdit.text())
+        self.ip_tid = int(self.lineEdit.text())
+        self.next_time = QTime.currentTime().addSecs(self.ip_tid)
         print('Satt ny ip-tid')
-        print(self.iptid)
-        self.nextTime = QTime.currentTime().addSecs(self.iptid)
+        print(self.ip_tid)
 
-    def nyIpActions(self):
+    def ny_ip_actions(self):
         print('Vi har fått ny ip')
 
     def visTid(self):
@@ -45,29 +49,28 @@ class MyWindow(QMainWindow, Ui_MainWindow):
        
         self.lbl_time_val.setText(current_time.toString('hh:mm:ss'))
         self.lbl_date_val.setText(current_date.toString('dd.MM.yyyy'))
-        self.lbl_lastipchecktime_val.setText(self.lastTime.toString())
-        self.lbl_nextipcheck_val.setText(self.nextTime.toString())
+        self.lbl_last_ipcheck_time_val.setText(self.last_time.toString())
+        self.lbl_next_ipcheck_val.setText(self.next_time.toString())
 
-        if current_time >= self.nextTime:  # Er det tid for ip-check?
-            self.lastTime = current_time  # lastTime = tiden for siste sjekk
-            self.nextTime = self.lastTime.addSecs(self.iptid)
+        if current_time >= self.next_time:  # Er det tid for ip-check?
+            self.last_time = current_time  # lastTime = tiden for siste sjekk
+            self.next_time = self.last_time.addSecs(self.ip_tid)
 
             ip = get('https://api.ipify.org').content.decode('utf8')
             self.lbl_lastip_val.setText(ip)
-            self.lbl_lastipsincedate_val.setText(self.lastIpDate)
-            print('sjekker-ip')
+            self.lbl_lastip_since_date_val.setText(self.last_ip_date)
+            print('sjekker-ip...')
 
             # Har vi ny IP?
-            if self.lbl_lastip_val.text() == self.lastIp:
+            if self.lbl_lastip_val.text() == self.last_ip:
                 print('Vi har samme ip')
             else:
-                self.lastIpDate = QDate.currentDate()
-                self.lastIpDate = self.lastIpDate.toString('dd.MM.yyyy')
-                self.lbl_lastipsincedate_val.setText(self.lastIpDate)
-                self.lastIpTime = current_time.toString('hh:mm:ss')  # lastIpTime er for når vi fikk siste IP
-                self.lbl_lastipsincetime_val.setText(self.lastIpTime)
-                self.lastIp = self.lbl_lastip_val.text()
-                self.nyIpActions()
+                self.last_ip_date = QDate.currentDate().toString('dd.MM.yyyy')   # lastIpate er for dagen vi fikk siste IP
+                self.lbl_lastip_since_date_val.setText(self.last_ip_date)
+                self.lastip_time = current_time.toString('hh:mm')             # lastIpTime er for når vi fikk siste IP
+                self.lbl_lastip_since_time_val.setText(self.last_ip_time)
+                self.last_ip = self.lbl_lastip_val.text()
+                self.ny_ip_actions()
 
 
 if __name__ == '__main__':
