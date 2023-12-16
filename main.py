3211a019001574bc49d883a351b4723c28d76045
tatanka22 <harald.grosvik@fritun.no>
2023-12-16 01:00:16 +0100

    diverse data testing

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import sys
 import os
 from dotenv import load_dotenv
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow,QCheckBox
-from PyQt5.QtCore import QTimer, QTime, QDate, QRegExp
+from PyQt5.QtWidgets import QApplication, QMainWindow, QCheckBox
+from PyQt5.QtCore import QTimer, QTime, QDate, QRegExp, QModelIndex, Qt, QAbstractTableModel
 from PyQt5.QtGui import QRegExpValidator, QStandardItem, QColor, QFont, QStandardItemModel
 from MyGui import Ui_MainWindow
 from domeneshop import Client
@@ -15,6 +15,11 @@ TOKEN = os.getenv('d_shop_token')
 SECRET = os.getenv('s_shop_secret')
 
 class StandardItem(QStandardItem):
+    list = []
+    colors = []
+    colors.append(QColor(0,0,0))
+    colors.append(QColor(0,0,255))
+
     def __init__(self, txt='', font_size=12, set_bold=False, color=QColor(0,0,0)):
         super().__init__()
         
@@ -25,9 +30,54 @@ class StandardItem(QStandardItem):
         self.setForeground(color)
         self.setFont(fnt)
         self.setText(txt)
-
-        self.check = QCheckBox('hallo')
    
+        StandardItem.list.append(self)
+        
+class aaaTableModel(QAbstractTableModel):
+    def __init__(self, data, parent=None):
+        super(TableModel, self).__init__(parent)
+        self._data = data
+        # default key
+        self.dict_key = 'key0'
+
+    def set_key(self, key):
+        self.beginResetModel()
+        self.dict_key = key
+        self.endResetModel()
+
+    def rowCount(self, QModelIndex_parent=None, *args, **kwargs):
+        return len(self._data[self.dict_key])
+
+    def columnCount(self, QModelIndex_parent=None, *args, **kwargs):
+        return len(self._data[self.dict_key][0])
+
+    def data(self, QModelIndex, int_role=None):
+        row = QModelIndex.row()
+        column = QModelIndex.column()
+        if int_role == Qt.DisplayRole:
+            return str(self._data[self.dict_key][row][column])
+    
+   
+class TableModel(QAbstractTableModel):
+    def __init__(self, data):
+        super(TableModel, self).__init__()
+        self._data = data
+
+    def data(self, index, role):
+        if role == Qt.DisplayRole:
+            # See below for the nested-list data structure.
+            # .row() indexes into the outer list,
+            # .column() indexes into the sub-list
+            return self._data[index.row()][index.column()]
+
+    def rowCount(self, index):
+        # The length of the outer list.
+        return len(self._data)
+
+    def columnCount(self, index):
+        # The following takes the first sub-list, and returns
+        # the length (only works if all rows are an equal length)
+        return len(self._data[0])
 
 class MyWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
@@ -47,8 +97,9 @@ class MyWindow(QMainWindow, Ui_MainWindow):
         self.ip_tid = 120
         self.lineEdit.setText("120")
 
-        # button for domains
+        # buttons for domains
         self.btn_domains.clicked.connect(self.get_domains)
+        self.btn_table.clicked.connect(self.get_table_domains)
 
         # Validator
         validator = QRegExpValidator(QRegExp(r'[0-9]+'))
@@ -67,7 +118,8 @@ class MyWindow(QMainWindow, Ui_MainWindow):
         self.last_ip_date = "12.12.12"
         self.last_ip_time = "00:00:00"
         self.last_ip = "192.192.192.192"
-    
+
+        
     def set_button_clicked(self):
         self.ip_tid = int(self.lineEdit.text())
         self.next_time = QTime.currentTime().addSecs(self.ip_tid)
@@ -78,22 +130,75 @@ class MyWindow(QMainWindow, Ui_MainWindow):
         # here we will put code for api
         pass
 
+    def gggg(self):
+        print("hoi")
+        
+        item_ind = self.treeView.currentIndex()
+        print(item_ind.data())
+        
+    def gg(self):
+        print("hoihoi")
+        item_ind = self.treeView.currentIndex()
+        
+        a=0
+        for item in StandardItem.list:
+            a +=1
+            #print (item.text() + str(a))
+            
+            if item.text() == str(item_ind.data()):
+                if item.foreground() == StandardItem.colors[0]:
+                    item.setForeground(StandardItem.colors[1])
+                else: 
+                    item.setForeground(StandardItem.colors[0])
+
+                #item.setForeground(QColor(0,0,255))
+
+        #self.min_record.setForeground(QColor(0,0,255))
+        
+        #print(item_ind.data())
+        #print(ab.data())
+        pass
+
+    def get_table_domains(self):
+        print('Clicked ')
+        
+        dataChunk = [
+          ["test.fritun.no", 9, 2],
+          ["harald.fritun.no", 0, 0],
+          ["hydro.fritun.no", 5, 0],
+          ["test.etase.no", 3, 2],
+          ["sjokolade.etase.no", 8, 9],
+        ]
+
+        dataChunk2 = [  {"Domene": "testdomene",
+                         "TTL": "3600", 
+                         "IP": "32.23.43.343" },
+                        {"Domene": "domenewes2", "TTL": "3600", "IP": "32.23.43.343" }]
+
+        tableModel = TableModel(dataChunk)
+        self.tableView.setModel(tableModel)
+       
     def get_domains(self):
        
         treeModel = QStandardItemModel()
+        #treeModel.setColumnCount(2)
+        
         rootNode = treeModel.invisibleRootItem()
         self.treeView.setModel(treeModel)
         self.treeView.setHeaderHidden(True)
 
-
+        self.treeView.clicked.connect(self.gggg)
+        self.treeView.doubleClicked.connect(self.gg)
+    
         self.domains = self.api_client.get_domains()
+       
         for domain in self.domains:
             print("*****")
 
             dom_txt = format(domain["domain"])
             domene = StandardItem(dom_txt, 12)
             rootNode.appendRow(domene)
-
+            #domene.setColumnCount(1)
             print("DNS records for {0}:" + dom_txt)
             print("id: {0}".format(domain["id"]))
             
@@ -101,20 +206,37 @@ class MyWindow(QMainWindow, Ui_MainWindow):
                 print(record["id"], record["host"], record["type"], record["data"])
                 
                 if record["type"] == 'A' and record["host"] != '@' :
-                    min_record = StandardItem( record["host"] + "." + dom_txt , 10)
-                    domene.appendRow(min_record)
+                    self.min_record = StandardItem( record['host'] + '.' + dom_txt,12 )
+                    test = []
+                    test.append(self.min_record)
+                    test2 = StandardItem('hallo')
+                    test3 = StandardItem('hadet')
+                    test.append(test2)
+                    test.append(test3)
+                    
+                    domene.insertRow(0,self.min_record)
 
-        myRec = {"host": "test",
+                    #domene.appendRow(self.min_record)
+                    
+                    
+        
+        myRec = {"host": "test56",
                 "ttl": 3600,
                 "type": "A",
-                "data": "62.24.36.27"}
-        #client.create_record(1834394, myRec)
+                "data": "62.24.36.27"
+                }
 
-       
-              
+        myRec2 = {"host": "test56",
+                "ttl": 3600,
+                "type": "A",
+                "data": "88.92.182.26"
+                }
+        # svar = self.api_client.create_record(1834394, myRec)
+        # print("ffffff" + str(svar))
+        #svar = self.api_client.modify_record(1834394, 5947892, myRec2)
+        #print("ffffff" + str(svar))
         
-
-
+        
     def visTid(self):
         current_time = QTime.currentTime()
         current_date = QDate.currentDate()
